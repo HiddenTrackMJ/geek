bc1c5e17533140d8136571d5fdcbf3bf6a283def
hewro <ihewro@163.com>
2020-02-05 16:52:49 +0800

    update

--- backend/src/main/resources/sql/geek.sql
+++ backend/src/main/resources/sql/geek.sql
@@ -1,9 +1,8 @@
 -- geek 后台表
 
--- user 用户信息
 create schema GEEK;
 
--- auto-generated definition
+-- user 用户信息
 CREATE SEQUENCE user_id_seq START WITH 1000;
 create table GEEK.USER
 (
@@ -16,35 +15,20 @@ create table GEEK.USER
 create unique index GEEK.USER_ID_UINDEX
     on GEEK.USER (ID);
 
+-- 房间信息
 CREATE SEQUENCE room_id_seq START WITH 1000;
 create table GEEK.ROOM
 (
     ID       BIGINT default room_id_seq.nextval PRIMARY KEY,
     TITLE    VARCHAR(200)  not null,
     DESC     VARCHAR(200),
-    LIVECODE VARCHAR(1000) not null
-);
-
-create unique index GEEK.ROOM_ID_UINDEX
-    on GEEK.ROOM (ID);
-
-CREATE SEQUENCE room_user_id_seq START WITH 1000;
-create table GEEK.ROOM_USER
-(
-    ID        BIGINT DEFAULT room_user_id_seq.nextval PRIMARY KEY,
-    ROOMID    BIGINT       not null,
-    USERID    BIGINT       not null,
-    MEMBERIDS VARCHAR(200) not null,
-    constraint ROOM_USER_ROOM_ID_FK
-        foreign key (ROOMID) references GEEK.ROOM (ID)
-            on update cascade on delete cascade,
+    LIVECODE VARCHAR(1000) not null,--4个成员 livecode
+    USERID    BIGINT       not null,--房主的用户id
+    MEMBERIDS VARCHAR(200) not null, --3个成员的id
     constraint ROOM_USER_USER_ID_FK
         foreign key (USERID) references GEEK.USER (ID)
             on update cascade on delete cascade
 );
 
-create unique index GEEK.ROOM_USER_ID_UINDEX
-    on GEEK.ROOM_USER (ID);
-
-
--- room 房间信息
+create unique index GEEK.ROOM_ID_UINDEX
+    on GEEK.ROOM (ID);
