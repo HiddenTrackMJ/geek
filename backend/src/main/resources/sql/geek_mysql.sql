b7b5f35fb86972c498ad19e06d54fa5c8352d31b
hewro <ihewro@163.com>
2020-02-07 18:24:28 +0800

    Update geek_mysql.sql

--- backend/src/main/resources/sql/geek_mysql.sql
+++ backend/src/main/resources/sql/geek_mysql.sql
@@ -10,8 +10,8 @@ create table `GEEK`.`ROOM`
     HOSTCODE  varchar(200)                                       not null,
     SERVERURL varchar(200)                                       not null,
     HOSTID    BIGINT                                             not null
-)ENGINE = InnoDB,auto_increment=1000;
-
+)ENGINE = InnoDB,AUTO_INCREMENT=1000;
+alter table ROOM AUTO_INCREMENT=1000;
 
 -- # 用户信息
 create table `GEEK`.`USER`
@@ -21,6 +21,7 @@ create table `GEEK`.`USER`
     PASSWORD VARCHAR(300)                                       not null,
     AVATAR   VARCHAR(300)
 )ENGINE = InnoDB,auto_increment=1000;
+alter table user AUTO_INCREMENT=1000;
 
 
 -- # 录像信息
@@ -33,3 +34,4 @@ create table `GEEK`.`VIDEO`
     FILENAME  VARCHAR(300)                                        not null,
     LENGTH    VARCHAR(100)                                        not null
 );
+alter table VIDEO AUTO_INCREMENT=1000;
