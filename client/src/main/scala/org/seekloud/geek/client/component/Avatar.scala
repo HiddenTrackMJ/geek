08c58f20e7ff6bb9e845cd5dd83c93a3bfd61132
hewro <ihewro@163.com>
2020-02-17 20:05:27 +0800

    update

---
+++ client/src/main/scala/org/seekloud/geek/client/component/Avatar.scala
@@ -0,0 +1,14 @@
+package org.seekloud.geek.client.component
+
+/**
+  * User: hewro
+  * Date: 2020/2/17
+  * Time: 15:09
+  * Description: 用户圆形头像
+  * refer: 参考 https://www.jianshu.com/p/779090da020f
+  */
+case class Avatar(
+
+){
+
+}
---
+++ client/src/main/scala/org/seekloud/geek/client/component/Message.scala
@@ -0,0 +1,11 @@
+package org.seekloud.geek.client.component
+
+/**
+  * User: hewro
+  * Date: 2020/2/17
+  * Time: 15:41
+  * Description: 每一条评论的构造
+  */
+case class Message(){
+
+}
